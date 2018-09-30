830d68fea54ed5f89f28eba5d04f938820b5b744
cami63830 <cami63830@gmail.com>
2018-09-29 21:09:13 -0300

    si

--- MODULO_2_EJERC_4/Program.cs
+++ MODULO_2_EJERC_4/Program.cs
@@ -10,6 +10,18 @@ namespace MODULO_2_EJERC_4
     {
         static void Main(string[] args)
         {
+
+            //Tarea 4: Crea un programa que calcule la diferencia(resta) entre 321 y 213
+            int V5, V6, R;
+            V5 = 321;
+            V6 = 213;
+            R = 0;
+
+            R = V5 - V6;
+            Console.WriteLine("Ejercicio 4: la resta de 321-213= {0}", R);
+            Console.ReadKey();
+
+
         }
     }
 }
